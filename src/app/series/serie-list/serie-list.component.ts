50155157af5d96a14bdd3799f61b6db36c21bea3
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-04-07 01:02:09 -0500

    Add average seasons calculation and display in SerieListComponent

--- src/app/series/serie-list/serie-list.component.ts
+++ src/app/series/serie-list/serie-list.component.ts
@@ -11,13 +11,23 @@ import { Serie } from '../serie.model';
 export class SerieListComponent implements OnInit{
 
   series: Serie[] = [];
+  averageSeasons: number = 0;
 
   constructor(private serieService: SerieService) {}
 
   ngOnInit(): void {
       this.serieService.getSeries().subscribe(data => {
-        this.series =data;
+        this.series = data;
+        this.averageSeasons = this.calculateAverage();
       });
+  }
+
+  calculateAverage(): number {
+    let average: number = 0;
+    this.series.forEach((serie)=> {
+      average += serie.seasons;
+    });
+    return average/this.series.length
   }
 
 }
