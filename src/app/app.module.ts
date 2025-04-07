1c147023ad83bf862970a6bcd99f5ec4b415bec8
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-04-07 00:44:57 -0500

    Add SeriesModule to AppModule imports for TV series management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SeriesModule } from './series/series.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SeriesModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
