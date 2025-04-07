da699405b2dadd692f246a72d0ddb138f0c5aeba
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-04-07 00:42:16 -0500

    Add Serie model and service for managing TV series data

---
+++ src/app/series/serie.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Serie } from './serie.model';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SerieService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+  getSeries(): Observable<Serie[]>{
+    return this.http.get<Serie[]>(this.apiUrl);
+  }
+}
