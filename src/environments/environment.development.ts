35761b6eb27b581dfd1f20063dbf44f8fdcfc735
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-04-13 13:43:24 -0500

    Update API URL in environment configuration for series data

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'series.json'
+    apiUrl: 'https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json'
 };
